10fcae3c6a4e8ac8b35e0a7bc76d34c2cbd71482
hammad-ali18 <hammadali0298@gmail.com>
2024-12-12 16:11:22 +0500

    Fix: rm seeds len

--- programs/simple_stake/src/constants.rs
+++ programs/simple_stake/src/constants.rs
@@ -2,10 +2,10 @@ use anchor_lang::prelude::*;
 
 #[constant]
 
-pub const SPL_POOL_STATE_SEED: &[u8;13] = b"init_spl_pool";
+pub const SPL_POOL_STATE_SEED: &[u8] = b"init_spl_pool";
 
-pub const SPL_STAKE_VAULT_SEED: &[u8;15]= b"stake_spl_vault";
+pub const SPL_STAKE_VAULT_SEED: &[u8]= b"stake_spl_vault";
 
-pub const SPL_REWARD_VAULT_SEED: &[u8;16]=b"reward_spl_vault";
+pub const SPL_REWARD_VAULT_SEED: &[u8]=b"reward_spl_vault";
 
-pub const SPL_STAKER_SEED:&[u8;15] =b"staker_spl_info";
+pub const SPL_STAKER_SEED:&[u8] =b"staker_spl_info";
