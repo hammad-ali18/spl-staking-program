3a11077ca287409d9c645f03db4b05dfbac3687c
hammad-ali18 <hammadali0298@gmail.com>
2024-12-12 18:22:38 +0500

    Fix: reward condition fixed

--- programs/simple_stake/src/instructions/claim.rs
+++ programs/simple_stake/src/instructions/claim.rs
@@ -1,3 +1,5 @@
+use std::ptr::read;
+
 use crate::{accounts, constants::*};
 use crate::{error::ErrorCode, state::StakerSplInfo, SplPoolState};
 use anchor_lang::prelude::*;
@@ -51,47 +53,64 @@ pub struct ClaimSpl<'info> {
     pub system_program: Program<'info, System>,
 }
 
+pub fn claim_spl(ctx: Context<ClaimSpl>) -> Result<()> {
+    let accounts = ctx.accounts;
+    let current_timestamp = Clock::get().unwrap().unix_timestamp as u64;
 
-    pub fn claim_spl(ctx:Context<ClaimSpl>) -> Result<()> {
-        let accounts = ctx.accounts;
-        let current_timestamp = Clock::get().unwrap().unix_timestamp as u64;
-        
-        //CHECK that staking duration  has ended
-        let staking_end_time = accounts.pool_state.init_timestamp + accounts.pool_state.stake_duration;
-        
-        if current_timestamp > staking_end_time {
-            // if the currenttime is beyond the staking end time
-            require!(
-                accounts.claimer_info.last_claimed_time + accounts.pool_state.claim_reward_interval <= staking_end_time,
-                ErrorCode::StakingDurationEnded
-            );
-        }
-        //Check that user has claimed first time
-        if accounts.claimer_info.last_claimed_time > 0 {
-            let next_allowed_claim =
-                accounts.claimer_info.last_claimed_time + accounts.pool_state.claim_reward_interval;
-            //if the current time is less than than the expected claim time so prevent the claimer
-            require_gte!(
-                current_timestamp,
-                next_allowed_claim,
-                ErrorCode::ClaimIntervalNotReached
-            );
-        }
+    //CHECK that staking duration  has ended
+    let staking_end_time = accounts.pool_state.init_timestamp + accounts.pool_state.stake_duration;
 
-        let reward = accounts.claimer_info.staker_staked_amount * accounts.pool_state.reward_rate;
-        require_gt!(reward, 0, ErrorCode::NoRewardsToClaim);
-        //update the 
-        accounts.claimer_info.last_claimed_time = current_timestamp;
-        let cpi_context = CpiContext::new(
-            accounts.token_program.to_account_info(),
-            token::Transfer {
-                from: accounts.stake_vault.to_account_info(),
-                to: accounts.claimer_token_acc.to_account_info(),
-                authority: accounts.pool_state.to_account_info(),
-            },
+    if current_timestamp > staking_end_time {
+        // if the currenttime is beyond the staking end time
+        require!(
+            accounts.claimer_info.last_claimed_time + accounts.pool_state.claim_reward_interval
+                <= staking_end_time,
+            ErrorCode::StakingDurationEnded
         );
-        token::transfer(cpi_context, reward)?;
-
-        Ok(())
+    }
+    //Check that user has claimed first time
+    if accounts.claimer_info.last_claimed_time > 0 {
+        let next_allowed_claim =
+            accounts.claimer_info.last_claimed_time + accounts.pool_state.claim_reward_interval;
+        //if the current time is less than than the expected claim time so prevent the claimer
+        require_gte!(
+            current_timestamp,
+            next_allowed_claim,
+            ErrorCode::ClaimIntervalNotReached
+        );
     }
 
+    
+    let reward = accounts
+        .claimer_info
+        .staker_staked_amount
+        .checked_mul(accounts.pool_state.reward_rate) 
+        .and_then(|v| v.checked_div(100)) 
+        .ok_or(ErrorCode::Overflow)?;
+ 
+    msg!("{}",reward);
+    accounts.claimer_info.last_claimed_time = current_timestamp;
+
+    let pool_state_authority = &[SPL_POOL_STATE_SEED];
+    let (_, pool_state_bump) =
+        Pubkey::find_program_address(pool_state_authority, &ctx.program_id);
+    //update the
+    let claim_transfer_ix = token::Transfer {
+        from: accounts.stake_vault.to_account_info(),
+        to: accounts.claimer_token_acc.to_account_info(),
+        authority: accounts.pool_state.to_account_info(),
+    };
+    let pool_state_signer_seeds: &[&[u8]] = &[SPL_POOL_STATE_SEED, &[pool_state_bump]];
+
+    // Create the CPI context for the token transfer
+    let signer_bind = [pool_state_signer_seeds];
+    let cpi_context = CpiContext::new_with_signer(
+        accounts.token_program.to_account_info(),
+        claim_transfer_ix,
+        &signer_bind,
+    );
+
+    token::transfer(cpi_context, reward)?;
+
+    Ok(())
+}
