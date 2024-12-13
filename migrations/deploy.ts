27e8620e085d22b74504b686d7204201533d2fdf
hammad-ali18 <hammadali0298@gmail.com>
2024-12-13 14:42:05 +0500

    chore: update migration script for staking program deployment

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -1,12 +1,18 @@
-// Migrations are an early feature. Currently, they're nothing more than this
-// single deploy script that's invoked from the CLI, injecting a provider
-// configured from the workspace's Anchor.toml.
-
-const anchor = require("@coral-xyz/anchor");
+const anchor = require('@coral-xyz/anchor');
 
 module.exports = async function (provider) {
   // Configure client to use the provider.
   anchor.setProvider(provider);
 
-  // Add your deploy script here.
+
+  const poolAccount = new anchor.PublicKey('');
+  // Deploy the program (if it's a new deployment)
+  const program = anchor.workspace.SimpleStake;
+  const tx = await program.rpc.initializeSpl({
+accounts:{
+poolState: poolAccount,
+}
+  });
+
+  console.log("Program deployed with tx:", tx);
 };
